a0ba4af58cf9be5351707a318411e7eeae76e0a0
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-17 22:54:31 +0200

    task 1 completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// phew… not a lot going on here. Please add some code!
+console.clear();
+
+const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
+
+bookmarkIcon.addEventListener("click", (event) => {
+  bookmarkIcon.classList.toggle("bookmark--active");
+});
+
+const answerBtn = document.querySelector('[data-js="answerBtn"]');
+const answerCard = document.querySelector('[data-js="card_answer"]');
+
+answerBtn.addEventListener("click", (event) => {
+  answerCard.classList.toggle("card__answer--active");
+});
